b7561c6f3884d1f14a2a36f88c37ed57264bfa32
advaitsoni <advait.soni@dal.ca>
2018-10-09 12:39:49 -0700

    made player list and cards for each player

--- 1.py
+++ 1.py
@@ -1,5 +1,6 @@
 import pyCardDeck as p
 def deck():
+	#generate card deck
 	card_type=['Heart','Club','Black','Spade']
 	card_no={'K':'King','Q':'Queen','J':'Jack',10:'ten',9:'nine',8:'eight'
 	,7:'seven',6:'six',5:'five',4:'four',3:'three',2:'two','A':'Ace'}
@@ -10,6 +11,15 @@ def deck():
 	return cards
 
 my_deck=p.Deck(deck())
-my_deck.shuffle()
-card=my_deck.draw()
-print(card)
+#my_deck.shuffle()	#shuffle deck
+#card=my_deck.draw()	#draw one card
+#print(card)
+player1=[]
+player2=[]
+player_list=[player1,player2]	#list of players
+
+for card in range(5):
+	for player in player_list:
+		my_deck.shuffle()
+		player.append(my_deck.draw())
+print(player_list)	#card list of each player
